99608002d909c5ea496914a545dd8da2270e4f4b
Tanaka Toshiaki <kamase.main@gmail.com>
2019-11-28 22:17:42 +0900

    edit jikken1_and_jikken2

--- jikken_1.py
+++ jikken_1.py
@@ -1,23 +1,51 @@
 import numpy as np
 import matplotlib.pyplot as plt
-
+import matplotlib.pyplot as ticker
 np.random.seed()
 N = 100000
 
+# 0か1しかないのに横軸に小数点が出てくるのは気持ち悪いので整数のみ出力されるようにする
+plt.gca().get_xaxis().set_major_locator(ticker.MaxNLocator(integer=True))
+
+plt.ylim([0,1])
+
 list = []
 
-delta = 0.1
-p = 0.5
-U = np.random.uniform(0.0,1.0,N)
-x = np.linspace(0,delta,N)
+delta = 1
+p = 0.1
+U = np.random.uniform(0.0,1.0,N) # 0.から1.までの乱数生成
 for i in U:
-    if i <= delta/p:
+    # 生成した乱数がdelta/p以下ならリストの終わりに1を追加
+    # そうでないならリストの終わりに0を追加
+    if i <= p:
         list.append(1)
     else:
         list.append(0)
 
 print(np.mean(list))
-print(len(x))
+print(np.var(list))
 
-plt.plot(x,list)
+count = 0
+kankaku_list = []
+for i in list:
+    print("-"*10)
+    count += 1
+    print(count)
+    print(i)
+    if i == 1:
+        kankaku_list.append(count)
+        count = 0
+
+
+
+counts,bins = np.histogram(kankaku_list)
+print(np.mean(kankaku_list))
+plt.hist(bins[:-1],bins,weights=counts/list.count(1))
+plt.grid(True)
+
+## 業務用 ライブラリの都合で日本語が豆腐になるので調整
+igfont = {'family':'Yu Gothic'}
+plt.title('実験１　δ=1,p=0.1に基づくベルヌーイ過程に基づく乱数の到着感覚の分布',**igfont)
+plt.xlabel('生成された乱数（０：未到着　１：到着）',**igfont)
+plt.ylabel('全体に占める割合',**igfont)
 plt.show()
--- jikken_2.py
+++ jikken_2.py
@@ -6,12 +6,37 @@ N = 100000
 U = np.random.uniform(0.0,1.0,N)
 ramda = 1
 
+# 6行目で作った一様乱数を指数分布に従った乱数に変換
 X = -1/ramda * np.log(1-U)
+
 X.sort()
 print(X)
 
-y,binEdges=np.histogram(data,bins=100)
-bincenters = 0.5*(binEdges[1:]+binEdges[:-1])
-p.plot(bincenters,y,'-')
+counts,bins = np.histogram(X)
+plt.hist(bins[:-1],bins,weights=counts/N)
 plt.grid(True)
+
+
+graph_list = []
+tmp = 0
+
+for i in counts:
+    print(i)
+    tmp += i
+    graph_list.append(tmp)
+
+graph_list = np.array(graph_list)/N
+bins = bins - (bins[1]-bins[0])/2 # 横幅の調整 これが無いと棒の右端に点がプロットされる
+graph_list = np.insert(graph_list,0,0)
+bins[0] = 0
+plt.plot(bins,graph_list,marker="o")
+
+
+## ライブラリの都合で日本語が豆腐になるので調整
+igfont = {'family':'Yu Gothic'}
+plt.title('実験２　指数分布に従う乱数と累積相対頻度のグラフ',**igfont)
+plt.xlabel('生成された乱数の値',**igfont)
+plt.ylabel('生成された量',**igfont)
+
 plt.show()
+#plt.savefig("fig/fig_jikken_2.png")
